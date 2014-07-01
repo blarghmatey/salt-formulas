4542bfb66ab6a0c4d6d9e76dbd788fa43fb8adf3
Tobias Macey <tmacey@boundlessnotions.com>
2014-07-01 14:03:39 -0400

    Updated gen_salt_formula to properly place files and create initial version

--- gen_salt_formula.py
+++ gen_salt_formula.py
@@ -1,23 +1,24 @@
 import os
 import sys
-import os.path as op
+# import os.path as op
 
-formula_name = ('_').join(sys.argv[1:])
+formula_name = ('-').join(sys.argv[1:])
 
 if len(formula_name) == 0:
     print("Please provide a formula name")
-    sys.exit(0)
+    sys.exit(1)
 
-root_path = op.join(os.getcwd(), formula_name)
+# root_path = os.getcwd()
 path_names = ['{0}/{0}'.format(formula_name)]
-os.makedirs(root_path, mode=0o755, exist_ok=True)
+os.makedirs(formula_name, mode=0o755, exist_ok=True)
 for path in path_names:
-    os.makedirs(op.join(root_path, path), mode=0o755, exist_ok=True)
+    os.makedirs(path, mode=0o755, exist_ok=True)
 
 file_names = ['{0}/pillar.example', '{0}/README.rst', '{0}/VERSION',
               '{0}/{0}/init.sls', '{0}/{0}/package_map.jinja']
 for fname in file_names:
-    os.mknod(op.join(root_path, fname.format(formula_name)), mode=0o644)
+    print(fname)
+    os.mknod(fname.format(formula_name), mode=0o644)
 
-with open('{}/VERSION'.format(formula_name), 'w') as version:
-    version.write('0.0.1')
+with open('{0}/VERSION'.format(formula_name), 'w') as version:
+    version.write('0.0.1\n')
