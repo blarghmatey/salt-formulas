888ae173e149a0752e7e49830d24e0f3d4423906
Tobias Macey <tmacey@boundlessnotions.com>
2014-07-01 14:41:34 -0400

    Updated gen_salt_formula to populate init.sls and package_map.jinja with boilerplate content

--- gen_salt_formula.py
+++ gen_salt_formula.py
@@ -17,8 +17,30 @@ for path in path_names:
 file_names = ['{0}/pillar.example', '{0}/README.rst', '{0}/VERSION',
               '{0}/{0}/init.sls', '{0}/{0}/package_map.jinja']
 for fname in file_names:
-    print(fname)
     os.mknod(fname.format(formula_name), mode=0o644)
 
 with open('{0}/VERSION'.format(formula_name), 'w') as version:
     version.write('0.0.1\n')
+
+with open('{0}/{0}/package_map.jinja'.format(formula_name), 'w') as pkg:
+    pkg.write(
+'''{{% set {0} = salt['grains.filter_by']({{
+    'Debian': {{
+        'pkgs': [
+        ]
+    }},
+    'RedHat': {{
+        'pkgs': [
+        ]
+    }},
+    'Arch': {{
+        'pkgs': [
+        ]
+    }}
+}}, merge=salt['pillar.get']('{0}:lookup')) %}}
+'''.format(formula_name))
+
+with open('{0}/{0}/init.sls'.format(formula_name), 'w') as init:
+    init.write(
+'''{{% from "{0}/package_map.jinja" import nginx with context %}}
+'''.format(formula_name))
