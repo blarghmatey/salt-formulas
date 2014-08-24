dae090c8fff277cc972096c8cf9cbcb8d9149907
Tobias Macey <tmacey@boundlessnotions.com>
2014-08-23 21:02:53 -0400

    Added the option to specify whether the skeleton is for a state, default is formula

--- gen_salt_formula.py
+++ gen_salt_formula.py
@@ -1,28 +1,43 @@
 import os
 import sys
+import argparse
 # import os.path as op
 
-formula_name = ('-').join(sys.argv[1:])
+parser = argparse.ArgumentParser(
+    description="Generate a salt formula skeleton")
+parser.add_argument('--state', dest='is_state', type=bool, default=False)
+parser.add_argument('formula_name', nargs='+', type=str)
+
+args = parser.parse_args()
+formula_name = ('-').join(args.formula_name)
 
 if len(formula_name) == 0:
     print("Please provide a formula name")
     sys.exit(1)
 
 # root_path = os.getcwd()
-path_names = ['{0}/{0}'.format(formula_name)]
+if args.is_state:
+    path_names = ['{0}/files'.format(formula_name)]
+else:
+    path_names = ['{0}/{0}'.format(formula_name)]
 os.makedirs(formula_name, mode=0o755, exist_ok=True)
 for path in path_names:
     os.makedirs(path, mode=0o755, exist_ok=True)
 
-file_names = ['{0}/pillar.example', '{0}/README.rst', '{0}/VERSION',
-              '{0}/{0}/init.sls', '{0}/{0}/package_map.jinja']
+if args.is_state:
+    file_names = ['{0}/init.sls', '{0}/package_map.jinja']
+else:
+    file_names = ['{0}/pillar.example', '{0}/README.rst', '{0}/VERSION',
+                  '{0}/{0}/init.sls', '{0}/{0}/package_map.jinja']
+
 for fname in file_names:
     os.mknod(fname.format(formula_name), mode=0o644)
 
-with open('{0}/VERSION'.format(formula_name), 'w') as version:
-    version.write('0.0.1\n')
+if not args.is_state:
+    with open('{0}/VERSION'.format(formula_name), 'w') as version:
+        version.write('0.0.1\n')
 
-with open('{0}/{0}/package_map.jinja'.format(formula_name), 'w') as pkg:
+with open(file_names[-1].format(formula_name), 'w') as pkg:
     pkg.write(
 '''{{% set {0} = salt['grains.filter_by']({{
     'Debian': {{
@@ -40,7 +55,7 @@ with open('{0}/{0}/package_map.jinja'.format(formula_name), 'w') as pkg:
 }}, merge=salt['pillar.get']('{0}:lookup')) %}}
 '''.format(formula_name))
 
-with open('{0}/{0}/init.sls'.format(formula_name), 'w') as init:
+with open(file_names[-2].format(formula_name), 'w') as init:
     init.write(
 '''{{% from "{0}/package_map.jinja" import {0} with context %}}
 '''.format(formula_name))
